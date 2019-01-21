12f29b82cd567f8933eae2c884934e56d8486daf
Natalie Woodard <nwoodard1@cnm.edu>
2019-01-21 10:17:39 -0700

    added purpose, audience and goal to project

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Milestone-1</title>
+	</head>
+	<body>
+		<h1><strong>Milestone-1</strong></h1>
+			<p>
+			<h3><strong>PURPOSE:</strong></h3>
+			<p>
+				I want to help my father's business by building him a creative, informative website. My father is the reason I am at this coding bootcamp and it's the least I could do for him.</p>
+		<h3>AUDIENCE:</h3>
+			<p>
+				People who are interested in solar energy are looking at this webpage. It could be someone just searching for information on it, or someone looking for a company to put panels on their house.
+			</p>
+		<h3>GOAL:</h3>
+			<p>
+				My goal for this webpage is to increase my father's client base and help his business sales.
+			</p>
+	</body>
