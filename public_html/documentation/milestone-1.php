6dab18e3e9f27ea60660ee638061f7c9e7b660c2
Natalie Woodard <nwoodard1@cnm.edu>
2019-01-21 12:55:16 -0700

    added persona to pwp milestone 1

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -10,12 +10,42 @@
 			<h3><strong>PURPOSE:</strong></h3>
 			<p>
 				I want to help my father's business by building him a creative, informative website. My father is the reason I am at this coding bootcamp and it's the least I could do for him.</p>
-		<h3>AUDIENCE:</h3>
+		<h3><strong>AUDIENCE:</strong></h3>
 			<p>
-				People who are interested in solar energy are looking at this webpage. It could be someone just searching for information on it, or someone looking for a company to put panels on their house.
+				People who are interested in alternative energy; and more specifically, solar energy are looking at this webpage. It could be someone just searching for information on it, or someone looking for a company to put panels on their house.
 			</p>
-		<h3>GOAL:</h3>
+		<h3><strong>GOAL:</strong></h3>
 			<p>
 				My goal for this webpage is to increase my father's client base and help his business sales.
 			</p>
+		<h2><strong>PERSONA:</strong></h2>
+			<h4><strong>Name:</strong></h4>
+				<p>John Maddox</p>
+			<h4><strong>Age:</strong></h4>
+				<p>47</p>
+			<h4><strong>Race:</strong></h4>
+				<p>Caucasian</p>
+		<h4><strong>Career:</strong></h4>
+				<p>Accountant for a non-profit farming cooperative</p>
+		<h4><strong>Relevant Technology:</strong></h4>
+				<p>John uses a windows 10 OS on his 2009 Dell Desktop computer. His cellphone is Android, a 2014 LG 4</p>
+		<h4><strong>Behaviors and Attitudes:</strong></h4>
+				<p>John recycles paper, plastic and cans, but not glass. John drives a 2005 Toyota Prius. He is in a book club and likes to ride his bicycle in the foothills in his free time.</p>
+		<h4><strong>Scenario:</strong></h4>
+				<p>John heard from someone at his last book club meeting that they had solar panels put on their house. They were excited to see their utility bills decrease and helping the environment. John thought this sounded like a great deal and wanted to look into getting some panels on his house, too.</p>
+		<h4><strong>Goal:</strong></h4>
+				<p>The goal of this webpage is to welcome Joh, inform him about alternative energy and solar energy. The option to contact someone with further questions or a service inquiry. Ultimately, the goal is to creat more business and revenue for the business.</p>
+		<h4><strong>Frustrations:</strong></h4>
+				<ul>
+					<li>
+						Make sure company is reliable</li>
+					<li>
+						Make sure reviews are positive</li>
+					<li>
+						Make sure prices are competitive</li>
+					<li>
+						Must own a house</li>
+					<li>
+						Must have the finances to spend on solar panels</li>
+				</ul>
 	</body>
