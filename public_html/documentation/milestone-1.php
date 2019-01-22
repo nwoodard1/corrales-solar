e7d5471c9964014f8dbb068969ed4aae3c5afb8e
Natalie Woodard <nwoodard1@cnm.edu>
2019-01-22 15:58:14 -0700

    added persona photo

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<title>Milestone-1</title>
 	</head>
-	<body>
+	<p>
 		<h1><strong>Milestone-1</strong></h1>
 			<p>
 			<h3><strong>PURPOSE:</strong></h3>
@@ -15,27 +15,18 @@
 				People who are interested in alternative energy; and more specifically, solar energy are looking at this webpage. It could be someone just searching for information on it, or someone looking for a company to put panels on their house.
 			</p>
 		<h3><strong>GOAL:</strong></h3>
-			<p>
-				My goal for this webpage is to increase my father's client base and help his business sales.
-			</p>
+		My goal for this webpage is to increase my father's client base and help his business sales.
 		<h2><strong>PERSONA:</strong></h2>
-			<h4><strong>Name:</strong></h4>
-				<p>John Maddox</p>
-			<h4><strong>Age:</strong></h4>
-				<p>47</p>
-			<h4><strong>Race:</strong></h4>
-				<p>Caucasian</p>
-		<h4><strong>Career:</strong></h4>
-				<p>Accountant for a non-profit farming cooperative</p>
-		<h4><strong>Relevant Technology:</strong></h4>
-				<p>John uses a windows 10 OS on his 2009 Dell Desktop computer. His cellphone is Android, a 2014 LG 4</p>
-		<h4><strong>Behaviors and Attitudes:</strong></h4>
-				<p>John recycles paper, plastic and cans, but not glass. John drives a 2005 Toyota Prius. He is in a book club and likes to ride his bicycle in the foothills in his free time.</p>
-		<h4><strong>Scenario:</strong></h4>
-				<p>John heard from someone at his last book club meeting that they had solar panels put on their house. They were excited to see their utility bills decrease and helping the environment. John thought this sounded like a great deal and wanted to look into getting some panels on his house, too.</p>
-		<h4><strong>Goal:</strong></h4>
-				<p>The goal of this webpage is to welcome Joh, inform him about alternative energy and solar energy. The option to contact someone with further questions or a service inquiry. Ultimately, the goal is to creat more business and revenue for the business.</p>
-		<h4><strong>Frustrations:</strong></h4>
+	<img src="john-maddox.jpg" alt="John Maddox" style="width:250px;height:250px;"/>
+		<p><strong>Name:</strong>John Maddox</p>
+		<p><strong>Age:</strong>47</p>
+		<p><strong>Race:</strong>Caucasian</p>
+		<p><strong>Career:</strong>Accountant for a non-profit farming cooperative</p>
+		<p><strong>Relevant Technology:</strong>John uses a windows 10 OS on his 2009 Dell Desktop computer. His cellphone is Android, a 2014 LG 4</p>
+		<p><strong>Behaviors and Attitudes:</strong>John recycles paper, plastic and cans, but not glass. John drives a 2005 Toyota Prius. He is in a book club and likes to ride his bicycle in the foothills in his free time.</p>
+		<p><strong>Scenario:</strong>John heard from someone at his last book club meeting that they had solar panels put on their house. They were excited to see their utility bills decrease and helping the environment. John thought this sounded like a great deal and wanted to look into getting some panels on his house, too.</p>
+		<p><strong>Goal:</strong>The goal of this webpage is to welcome Joh, inform him about alternative energy and solar energy. The option to contact someone with further questions or a service inquiry. Ultimately, the goal is to creat more business and revenue for the business.</p>
+		<p><strong>Frustrations:</strong>
 				<ul>
 					<li>
 						Make sure company is reliable</li>
@@ -47,5 +38,6 @@
 						Must own a house</li>
 					<li>
 						Must have the finances to spend on solar panels</li>
-				</ul>
+	</ul>
 	</body>
+	</html>
