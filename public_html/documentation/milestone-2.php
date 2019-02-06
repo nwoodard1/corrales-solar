5f8a1862d8dcbb20321b836f9169dfb820209ff4
Natalie Woodard <nwoodard1@cnm.edu>
2019-02-05 21:03:34 -0700

    Added content strategy and image link to my wireframes to milestone 2 file

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Milestone-2</title>
+	</head>
+	<body>
+		<h2>
+			Content Strategy
+		</h2>
+		<strong>Logo:</strong>
+			This is where the company logo will go.
+		<p>
+		<strong>Toolbar:</strong>
+			The toolbar has four buttons, all of which drop-down when clicked on. The design button drop-down will contain a short explanation related to the design of the product options the company has to offer. The services button drop-down will describe the services the company provides. The installation button drop-down will provide a brief description of the installation options available to the potential client. The FAQ button drop-down will contain the top 8 most frequently asked questions. The header will have the company's name.</p>
+		<p>
+		<strong>Welcome Section: </strong>
+			 This section will contain a brief, warm greeting from the owner of the company.</p>
+		<p>
+		<strong>Mission Statement:</strong>
+			 This will be the section where the company's mission statement will go.</p>
+		<p>
+		<strong>Renewable Energy Facts: </strong>
+			 This section is for people to understand the basics of renewable energy that they should know before they ask any questions.</p>
+		<p>
+		<strong>Photo Gallery: </strong>
+		The cover-flow container is for a slideshow of photos which will move when hovered over with a cursor or a finger or thumb (depending on if access is via mobile or not).</p>
+		<p>
+		<strong> Business Location: </strong>
+		The GPS map will show the location of the business on it. </p>
+		<p>
+		<strong>Contact Box: </strong>
+		The contact box will have a subject heading line and a text box. This is where the client can send a message to the business for inquiries about services or more questions. This message will be sent to the owner's e-mail account.</p>
+		<p>
+		<strong>Contact Us: </strong>
+		This text-box contains all of the information a potential client may need to contact the business. It will contain the address, phone number and e-mail for the company.</p>
+		<p>
+			<img src="pwp-ms2a-wireframe.bmpr"/> </p>
+	</body>
