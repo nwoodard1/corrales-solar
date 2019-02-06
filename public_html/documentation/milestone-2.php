150f7b21e089e6a07ce5742a6746ac4038d54e39
Natalie Woodard <nwoodard1@cnm.edu>
2019-02-06 05:59:45 -0700

    Trying to get my wireframes file to work inside php storm. Changed it to a PDF and it's still not working.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -35,6 +35,5 @@
 		<strong>Contact Us: </strong>
 		This text-box contains all of the information a potential client may need to contact the business. It will contain the address, phone number and e-mail for the company.</p>
 		<p>
-			<img src="./pwp-ms2a-wireframe.bmpr"/>
-			<img src="./pwp-ms2amobile-wireframe.bmpr"/> </p>
+			<img src="./pwp-ms2a-wireframe.pdf"/>
 	</body>
