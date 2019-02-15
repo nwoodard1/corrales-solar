700285150871fb07c2feed3566410d13bdf79cca
Natalie Woodard <nwoodard1@cnm.edu>
2019-02-15 16:44:05 -0700

    Began webpage, created navbar

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8">
-		<title>Milestone-2b</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!---Custom CSS--->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+		<!---Bootstrap CSS--->
+		<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-gjzzqfgwb1qttn6wy59fff1bugjplsa9dkkmp0dgimdm4iymj70gzwkybi706tws" crossorigin="anonymous">
+
+
+				<!---JS, Popper.js, and jQuery--->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 	</head>
-	<body>
-	</body>
+<body>
+	<!---header--->
+	<header>
+	<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light border rounded my-3 border border-secondary">
+			<a class="navbar-brand" href="#">Go To</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+				<ul class="navbar-nav ml-auto mt-2 mt-md-0">
+					<li class="nav-item active">
+
+						<a class="nav-link" href="#">Design <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Installation</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Services</a>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Renewable Facts</a>
+					<li class="nav-item">
+						<a class="nav-link" href="#">FAQ</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+	</header>
+	<!--Carousel of images-->
+	<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="..." class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item">
+				<img src="..." class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item">
+				<img src="..." class="d-block w-100" alt="...">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+</body>
+</html>
