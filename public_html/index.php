87253ba412d6c8d59c7096b8b61d2ba121940503
Natalie Woodard <nwoodard1@cnm.edu>
2019-03-02 21:51:04 -0700

    Added images to site, adjusted content of welcome section. Didn't get very far on that part..

--- public_html/index.php
+++ public_html/index.php
@@ -5,11 +5,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<!---Custom CSS--->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 				integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-		<!---Bootstrap CSS--->
-		<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-gjzzqfgwb1qttn6wy59fff1bugjplsa9dkkmp0dgimdm4iymj70gzwkybi706tws" crossorigin="anonymous">
 
 
 				<!---JS, Popper.js, and jQuery--->
@@ -36,13 +34,7 @@
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<!---end of contact form link---->
-
-		<!-----GOOGLE MAPS API------->
-
-		<!---end google maps API--->
 	</head>
-
-
 	<body>
 		<!---header--->
 		<header>
@@ -75,16 +67,14 @@
 		</header>
 
 		<!--Card with logo-->
-
-
-		<div class="container ">
-
-			<div class="row text-center d-flex justify-content-right my-5">
+		<div class="container-welcome">
+			<div class="row text-center d-flex justify-content-center m-auto">
 				<div class="col-sm-1 p-1">
 					<img class="rounded-circle img-fluid my-3" src="images/e.jpg" alt="company logo">
 				</div>
-				<div class=" col-10 text-wrap my-2 py-2"
+				<div class=" col-10 text-wrap my-2 py-2">
 				<h1><strong>Welcome to Corrales Solar powered by Advantage Energy Solutions, LLC.</strong></h1>
+				</div>
 			</div>
 		</div>
 		<div class="container-fluid">
@@ -93,12 +83,19 @@
 		</div>
 
 		<!---mission statement--->
+		<div id="missionStatement"
 		<div class="container-fluid">
-			<div class="row text-center d-flex justify-content-center">
-				<div class="col-sm-9">
-					<div class="container my-auto py-5">
-						<p class="mission statement">
-							Welcome to our official website. Please look around and contact us with any questions or comments.
+			<div class="row justify-content-center">
+				<div class="col-md-6">
+					<img src="images/corralessolar1.jpg" height="400" width="600"
+				</div>
+			</div>
+
+			<p class="missionStatement">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-md-8 text-center d-flex float">
+								Welcome to our official website. Please look around and contact us with any questions or comments.
 							We believe that anyone can be a part of energy conservation on a small scale, and we would like to
 							help people reach this goal at an affordable cost. Our mission is providing clear customer
 							education, thoughtful design, fair pricing with quality components, careful installation, and
@@ -117,6 +114,9 @@
 			<div class="row text-center d-flex justify-content-center">
 				<div class="col-sm-9">
 					<div class="container my-auto py-5">
+						<div class="col-md-6">
+							<img src="images/corralessolar2.jpg" height="400" width="600"
+						</div>
 						<p class="Design">
 						<h2>Design</h2>We work with owners to size a solar system that will offset as much power as possible, but no more than necessary, using the National Renewable energy Laboratory's PV Watts estimation program.
 					</div>
@@ -133,6 +133,9 @@
 				<div class="col-sm-9">
 					<div class="container my-auto py-5">
 						<p class="Installation">
+						<div class="col-md-6">
+							<img src="images/corralessolar3.jpg" height="400" width="600"
+						</div>
 						<h2>Installation</h2>
 						All Advantage Energy Solutions, LLC and Corrales Solar designs are installed through a contract between the homeowner and Compass Electric Company, a licensed and insured Albuquerque company with extensive experience in solar installation.
 					</div>
@@ -144,8 +147,11 @@
 		<hr class="my-4">
 <section id="services">
 		<div class="container-fluid">
-			<div class="row text-center d-flex justify-content-center">
+			<div class="row text-center text-wrap d-flex justify-content-center">
 				<div class="col-sm-9">
+					<div class="col-md-6">
+						<img src="images/corralessolar4.jpg" height="400" width="600"
+					</div>
 					<div class="container my-auto py-5">
 						<p class="Services">
 						<h2>Services</h2>Residential or Commercial
@@ -157,25 +163,27 @@
 </section>
 		<!--A fancy line-->
 		<hr class="my-4">
+		<div class="row text-center d-flex justify-content-center">
+		<h2>Renewable Energy Facts</h2>
+		</div>
+
+
 <section id="renewableEnergyFacts">
 		<div class="container-fluid">
-			<div class="row text-center d-flex justify-content-center">
-				<div class="col-sm-9">
-					<div class="container my-auto py-5">
+			<div class="row text-left d-flex justify-content-left">
+				<div class="col-md-12">
+					<div class="col-md-6">
+						<img src="images/corralessolar5.jpg" height="400" width="600"
+					</div>
+					<div class="container">
 						<p class="Renewable Energy Facts">
-						<h2>Renewable Energy Facts</h2>This is where information about renewable energy facts will go.
-						Cat not kitten around demand to be let outside at once, and expect owner to wait for me as i think
-						about it fight an alligator and win eat too much then proceed to regurgitate all over living room
-						carpet while humans eat dinner kitten is playing with dead mouse so if it fits i sits adventure
-						always. Meow all night having their mate disturbing sleeping humans. Wake up wander around the house
-						making large amounts of noise jump on top of your human's bed and fall asleep again lick face hiss at
-						owner, pee a lot, and meow repeatedly scratch at fence purrrrrr eat muffins and poutine until owner
-						comes back so nya nya nyan and walk on car leaving trail of paw prints on hood and windshield attack
-						the child bathe private parts with tongue then lick owner's face. Eat all the power cords cat slap dog
-						in face so suddenly go on wild-eyed crazy rampage so mark territory, but i like fish hunt anything
-						that moves. Get suspicious of own shadow then go play with toilette paper eat all the power cords
-						attack dog, run away and pretend to be victim step on your keyboard while you're gaming and then turn
-						in a circle caticus cuteicus chew foot love to play with owner's hair tie.
+						<ul class="justify-content-center col-md-12">
+							<li><p> Renewable energy comes from a natural source that is theoretically inexhaustible, and includes wind, solar, biomass, tidal, wave, and hydroelectric. Of these, solar is the most versatile and simplest to site on a residence.</p></li>
+						<li><p> The electrification of the United States was accomplished in the 1920s and 1930s through the development of centralized large-scale power plants and a network of distribution power lines. Large hydroelectric facilities and nuclear power plants were added to the grid through the 1960s and 1970s. All of the large power generating facilities were designed to generate and sell power to the consumer, and the utilities that own them are highly-regulated monopolies.</p></li>
+						<li><p> Changes to Federal energy policy in 1992 and 2005 encouraged renewable sources, and allowed generation of power from sources such as wind and solar farms. This changed the flow of electrons for the first time.</p></li>
+						<li><p>Small-scale solar remained a niche power source for off-grid use until the invention of the grid-tied inverter, which synchronized solar power with utility grid power, and allowed excess power at a residence to flow onto the grid. This ability to offset utility bills fueled exponential growth in small residential systems, growing distributed rather than centralized power production.</p></li>
+						<li><p>Battery storage of excess power is becoming more affordable and useful as a means of backup power and/or a way to shift load to a less expensive time of day.</p></li>
+						</ul>
 					</div>
 				</div>
 			</div>
@@ -189,26 +197,29 @@
 
 <section id="FAQ">
 	<div class="row text-left d-flex justify-content-left">
-		<div class="container-fluid">
+		<div class="containerFaq-fluid">
 			<div class="row text-left d-flex justify-content-left">
-				<div class="col-sm-9">
-					<div class="container my-auto py-5">
+				<div class="col-md-12">
+					<div class="col-md-6">
+						<img src="images/corralessolar6.jpg" height="400" width="600"
+					</div>
+					<div class="container">
 						<p class="FAQ">
 
-							<p>Q: How does solar PV work?</p>
-							<p>A: Sunlight hits the solar panels, where photons are converted to a direct current (DC). The direct current is routed to an inverter, which changes it to alternating current (AC). In a grid-tied system, the inverter locks onto the utility power and frequency, and, if more power is being made than is being used, the power flows onto the grid.</p>
-							<p>Q: What is net metering?</p>
-							<p>A: A qualified generation system must be allowed to push power onto the grid. Net metering is the process of compensation for the owner of the generator. Different utilities interpret net metering differently. In the PNM Small Solar Program, with inverter(s) no larger than 10kW AC, power that is pushed onto the grid becomes a credit, which may be used at a later date. The PNM Large Solar Program does not have the credit carryover provision.</p>
-							<p>Q: What is a kilowatt hour?</p>
-							<p>A: A kilowatt hour (kWh) is the basic unit of power consumption. Your utility bill provides a total of how many kWh you used in a month. A kilowatt (kW) is 1,000 watts, and a kilowatt hour is 1,000 watts of power used for one  hour. It may be compared to ten 100-watt lamps on for one hour.</p>
-							<p>Q: How are PV systems sized?</p>
-							<p>A: A PV system is generally sized in kilowatts (kW) of DC power. The size in watts of each panel times the number of panels divided by 1000 is the kW size of a system.</p>
-							<p>Q: How do I know how much PV I need?</p>
-							<p>A: Within the PNM Small PV Program, the economic sweet spot is an offset of all of your annual use, or a bit less. First total twelve months of your monthly usage in kWh to get your annual usage. A good rule of thumb in the Albuquerque area is that a PV system will produce 5 kWh per DC kW of system size per day. Divide the total annual kWh by 5, then by 365 to come up with an estimated of the size in DC kW. For example, the average PNM customer averages 600 kWh/month, so 12 X 600 = 7200 kWh/year. 7200 / 5 / 365 = 3.94 kW.</p>
-							<p>Q: Can't I oversize my system and have PNM pay me for power?</p>
-							<p>A: It does not work out economically to oversize, under either of PNM's Solar Programs. With the Small Program, you wind up with a credit that is only good if you use it, and with the Large Program, you are paid for excess power at PNM's wholesale cost.</p>
-							<p>Q: How much does a PV system cost?</p>
-							<p>A: The cost of a PV system depends on a few factors, such as the price of panels, the type of mount, and the type of inverter(s). Premium high-efficiency panels can be expensive, and are best used where space for panels is limited. Any roof mount is less expensive than a ground mount, and flush mounts on a pitched roof are less expensive than ballast mounts on a flat roof. Microinverters are cost effective for a small system, but become much more expensive than string inverters in large system. Large systems in general are less expensive per watt than small systems. A ballpark working number would be in the range of $2.25 to $3.25/watt.</p>
+						<p><strong>Q:</strong> How does solar PV work?</p>
+						<p><strong>A:</strong> Sunlight hits the solar panels, where photons are converted to a direct current (DC). The direct current is routed to an inverter, which changes it to alternating current (AC). In a grid-tied system, the inverter locks onto the utility power and frequency, and, if more power is being made than is being used, the power flows onto the grid.</p>
+						<p><strong>Q:</strong> What is net metering?</p>
+						<p><strong>A:</strong> A qualified generation system must be allowed to push power onto the grid. Net metering is the process of compensation for the owner of the generator. Different utilities interpret net metering differently. In the PNM Small Solar Program, with inverter(s) no larger than 10kW AC, power that is pushed onto the grid becomes a credit, which may be used at a later date. The PNM Large Solar Program does not have the credit carryover provision.</p>
+						<p><strong>Q:</strong> What is a kilowatt hour?</p>
+						<p><strong>A:</strong> A kilowatt hour (kWh) is the basic unit of power consumption. Your utility bill provides a total of how many kWh you used in a month. A kilowatt (kW) is 1,000 watts, and a kilowatt hour is 1,000 watts of power used for one  hour. It may be compared to ten 100-watt lamps on for one hour.</p>
+						<p><strong>Q:</strong> How are PV systems sized?</p>
+						<p><strong>A:</strong> A PV system is generally sized in kilowatts (kW) of DC power. The size in watts of each panel times the number of panels divided by 1000 is the kW size of a system.</p>
+						<p><strong>Q:</strong> How do I know how much PV I need?</p>
+						<p><strong>A:</strong> Within the PNM Small PV Program, the economic sweet spot is an offset of all of your annual use, or a bit less. First total twelve months of your monthly usage in kWh to get your annual usage. A good rule of thumb in the Albuquerque area is that a PV system will produce 5 kWh per DC kW of system size per day. Divide the total annual kWh by 5, then by 365 to come up with an estimated of the size in DC kW. For example, the average PNM customer averages 600 kWh/month, so 12 X 600 = 7200 kWh/year. 7200 / 5 / 365 = 3.94 kW.</p>
+						<p><strong>Q:</strong> Can't I oversize my system and have PNM pay me for power?</p>
+						<p><strong>A:</strong> It does not work out economically to oversize, under either of PNM's Solar Programs. With the Small Program, you wind up with a credit that is only good if you use it, and with the Large Program, you are paid for excess power at PNM's wholesale cost.</p>
+						<p><strong>Q:</strong> How much does a PV system cost?</p>
+						<p><strong>A:</strong> The cost of a PV system depends on a few factors, such as the price of panels, the type of mount, and the type of inverter(s). Premium high-efficiency panels can be expensive, and are best used where space for panels is limited. Any roof mount is less expensive than a ground mount, and flush mounts on a pitched roof are less expensive than ballast mounts on a flat roof. Microinverters are cost effective for a small system, but become much more expensive than string inverters in large system. Large systems in general are less expensive per watt than small systems. A ballpark working number would be in the range of $2.25 to $3.25/watt.</p>
 					</div>
 				</div>
 			</div>
@@ -218,18 +229,20 @@
 
 
 		<!-----Contact Form----->
-<form>
-		<section id="contactWrapper">
-			<div class="container">
 
-
+		<section id="container-form-map">
+			<div id="map" class="row justify-content-center col-md-12">
 				<iframe src="https://www.google.com/maps/d/embed?mid=1WPkErNGnttL9KbCcmeCneZYMqH4Jgm1N" width="480" height="360"></iframe>
-
 			</div>
+
+			<div class="contactWrapper">
+				<div class="form-wrapper">
+			</div>
+				<form>
 			<div class="container contact-form border rounded text-white py-5 px-5 text-wrap">
 				<form id="contactMe" action="php/mailer.php" method="post">
-					<h3>Send Me a Message</h3>
 					<div class="row">
+						<h3 class="text-center col-12 text-dark" ><strong>Contact Us</strong></h3>
 						<div class="col-md-6">
 							<div class="form-group">
 								<input type="text" name="contactName" class="form-control" placeholder="Your Name *" value=""/>
@@ -251,7 +264,7 @@
 
 								<textarea name="txtMsg" class="form-control" placeholder="Your Message *"
 											 style="width: 100%; height: 150px;"></textarea>
-							<input type="submit" name="btnSubmit" class="btnContact" value="Send Message"/>
+							<input type="submit" name="btnSubmit" class="btnContact m-2" value="Send Message"/>
 						</div>
 							</div>
 
@@ -259,8 +272,8 @@
 						</div>
 						</div>
 							</div>
-<div class="row justify-content-center py-1 my-2">
-	<div class="g-recaptcha col-md-6" data-sitekey="6LfKEpQUAAAAAA98OHTFmk9jpsrPh170gSaih6u6"></div>
+<div class="row justify-content-center col-md-12">
+	<div class="g-recaptcha" data-sitekey="6LfKEpQUAAAAAA98OHTFmk9jpsrPh170gSaih6u6"></div>
 </div>
 		</section>
 </form>
@@ -270,11 +283,18 @@
 
 		<!-----footer-->
 		<footer>
-			<div class="container-fluid bg-dark pt-1 pb-1 text-white-50">
+			<div class="container-fluid bg-dark py-5 text-white-50">
 				<footer class="page-footer">
 					<div class="row">
 						<div class="col">
-							<p class="text-left">Contact Us: (505) 615-9388 Corrales, NM corrales.solar@gmail.com
+							<div class="row">
+							<p class="text-center col-4">(505) 615-9388</p>
+								<p class="text-center col-4">
+									Corrales, NM</p>
+								<p class="text-center col-4">
+									corrales.solar@gmail.com
+								</p>
+							</div>
 						</div>
 					</div>
 			</div>
